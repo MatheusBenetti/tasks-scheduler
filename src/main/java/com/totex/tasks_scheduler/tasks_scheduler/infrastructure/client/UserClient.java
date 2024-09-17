6f8149e21def77cd2a42be93ae86f924e28b3c6f
MatheusBenetti <mbbenetti1@gmail.com>
2024-09-17 14:17:49 -0300

    Add a method to get jwt token from user login

---
+++ src/main/java/com/totex/tasks_scheduler/tasks_scheduler/infrastructure/client/UserClient.java
@@ -0,0 +1,15 @@
+package com.totex.tasks_scheduler.tasks_scheduler.infrastructure.client;
+
+
+import com.totex.tasks_scheduler.tasks_scheduler.business.dto.UserDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "user", url = "${user.url}")
+public interface UserClient {
+
+    @GetMapping("/user")
+    UserDto getUserByEmail(@RequestParam("email") String email, @RequestHeader("Authorization") String token);
+}
