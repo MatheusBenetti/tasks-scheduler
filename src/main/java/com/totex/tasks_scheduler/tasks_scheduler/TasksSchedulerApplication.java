8d592d079b4d40301b5edbc797f11b148f3f1fc7
MatheusBenetti <mbbenetti1@gmail.com>
2024-09-17 13:45:47 -0300

    first commit

---
+++ src/main/java/com/totex/tasks_scheduler/tasks_scheduler/TasksSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.totex.tasks_scheduler.tasks_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/totex/tasks_scheduler/tasks_scheduler/TasksSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totex.tasks_scheduler.tasks_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
